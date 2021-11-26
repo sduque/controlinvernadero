5a7f1efd2246173162c46396130520e73898ba21
sduque <91767084+sduque@users.noreply.github.com>
2021-11-26 17:35:30 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -33,8 +33,29 @@ $app->get('/pruebaGet/{nombre}', function($nombre) use($app) {
   $app['monolog']->addDebug('logging output.');
   return $nombre;
 });
-$app->post('/rutaPost', function(Request $request) use($app) {
-  
-  return "te has conectado a una ruta POST";
+$app->post('/guardarAmbiente', function(Request $request) use($app) {
+ 
+  $temperatura = $request->get('temperatura');
+  $humedad = $request->get('humedad');
+  $luz = $request->get('luz');
+
+		$data = array(
+			"fecha"=>date('Y-m-d H:i:s'),
+			"temperatura" => $temperatura,
+			"humedad" => $humedad,
+			"luz" => $luz
+		);
+	}
+	else{
+		return "Tabla no válida";
+	}
+
+
+	$dbconn = pg_pconnect("ec2-44-198-236-169.compute-1.amazonaws.com port=5432 dbname=dduoiujimkorgl user=qtxzkhxiiwythp password=85d35b4c909dc5ee7f2ae30c73fa03018aea6b400f011ecf75696e84404c0ef9");
+	$respuesta = pg_insert($dbconn, "ambiente", $data);
+
+	echo $respuesta; echo "<br><br>";
+
+   	return "OK";
 });
 $app->run();
